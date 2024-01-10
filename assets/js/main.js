414186196f2336b8e10ff79f866a1cc9c4b4211b
maaz bin Hassan <mleghari64@gmail.com>
2024-01-10 17:00:17 +0500

    summary

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,3 +260,19 @@
   new PureCounter();
 
 })()
+document.addEventListener('DOMContentLoaded', function () {
+  const darkModeToggle = document.querySelector('.dark-mode-toggle-btn');
+  const body = document.body;
+
+  darkModeToggle.addEventListener('click', function () {
+    body.classList.toggle('dark-mode');
+    // Save user preference to local storage
+    localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
+  });
+
+  // Check user preference from local storage on page load
+  const isDarkMode = localStorage.getItem('darkMode') === 'true';
+  if (isDarkMode) {
+    body.classList.add('dark-mode');
+  }
+});
